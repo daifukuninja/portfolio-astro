71f29a5ea1a321e5b886ff83fb661e3208d2f223
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-05-18 17:34:59 +0000

    fix: saosのアニメーションを廃止

--- src/components/BottomArrow.svelte
+++ src/components/BottomArrow.svelte
@@ -1,15 +1,47 @@
 <script lang="ts">
-    import Saos from "saos";
 </script>
 
 <div class="wrapper">
-    <Saos animation={"from-left 0.5s 5 cubic-bezier(0.35, 0.5, 0.65, 0.95) both"}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(256, 256, 256, 1);"><path d="M16.939 10.939 12 15.879l-4.939-4.94-2.122 2.122L12 20.121l7.061-7.06z"></path><path d="M16.939 3.939 12 8.879l-4.939-4.94-2.122 2.122L12 13.121l7.061-7.06z"></path></svg>
-    </Saos>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 24 24"
+        style="fill: rgba(192, 192, 192, 1);"
+        class="arrow"
+    >
+        <path
+            d="M16.939 10.939 12 15.879l-4.939-4.94-2.122 2.122L12 20.121l7.061-7.06z"
+        />
+        <path
+            d="M16.939 3.939 12 8.879l-4.939-4.94-2.122 2.122L12 13.121l7.061-7.06z"
+        />
+    </svg>
 </div>
 
 <style>
+    .arrow path {
+        -webkit-animation: stroke 0.8s ease-in-out infinite alternate;
+        animation: stroke 0.8s ease-in-out infinite alternate;
+    }
 
+    @-webkit-keyframes stroke {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    @keyframes stroke {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
     .wrapper {
         position: relative;
         top: 10%;

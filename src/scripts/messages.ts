a87605248923929224684a19b13c22969bbf9c41
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-05-18 13:12:52 +0000

    add: script、css、画像を移管

---
+++ src/scripts/messages.ts
@@ -0,0 +1,77 @@
+export const introduction = {
+    jp: `私の自己紹介, 製作物や執筆記事, 業務で身に付けたスキルをご案内させていただきます。
+    なにか１つでもご参考になるものをお持ち帰りいただければ幸いです。`,
+    en: `On this site, I will introduce myself, my productions, 
+    articles I have written, and the skills I have acquired in my work.`,
+}
+
+export const aboutMe = {
+    jp: `MIHARA Keishi は日本のフリーランスWebエンジニア, バックエンドプログラマ, デスクトップアプリケーションエンジニア, 
+    糖質制限者, 会社員, 団塊ジュニア, テックライター, スノーボーダー, 運転手, 
+    元 Lotus Notes/Domino アプリケーションデータベースエンジニアでExcelマクロ書き。
+
+    ３児の父でもあり, 家庭においては主に掃除, 残飯処理, 害虫駆除, 高所作業などを担当する。
+    
+    茨城県水戸市出身, 茨城県水戸市育ち, 埼玉県本庄市経験者。小学校時代の得意科目は理科, 図工。
+    
+    
+    株式会社 Contribe 所属。`.replace(/\n/g, "<br />"),
+    en: `MIHARA Keishi is a Japanese freelance web engineer, backend programmer, desktop application engineer, 
+    and dieter, Company Employee, Baby Boomer, Tech Writer, Snowboarder, Chauffeur, 
+    Previously Lotus Notes/Domino Application Database Engineer, and Excel Macro Programmer.
+    
+    Father of 3 children.
+
+
+    Born in Mito City, Ibaraki Prefecture, raised in Mito City, Ibaraki Prefecture, and experienced in Honjo City, Saitama Prefecture.
+
+    His best subjects in elementary school were science and arts and crafts.(not good at mathematics)
+    
+    
+    He is a member of Contribe Co.,Ltd.`,
+}
+
+export const skills = {
+    jp: `Python, C#, VB系の言語を中心に、デスクトップアプリケーション, エンドユーザ業務効率化の支援, ツール類の設計・開発を10年以上経験してきました。
+    
+    直近ではWebシステムやスマホアプリのバックエンド開発に関わらせていただき, フロントエンドは PHP(Laravel), バックエンドは Go での実装を担当しています。
+
+    エンドユーザと直接関わる機会が多かったので, 常にユーザ目線に立つことを忘れず, 画面や機能, API ならインターフェイスを形にするとき, どうすればクライアントが使いやすくなるかを常に考えるようにしています。`.replace(/\n/g, "<br />"),
+    en: ``,
+}
+
+export const otherSkills = {
+    jp: `その他, 経験のある技術群についてはこちらをご覧ください。`,
+    en: `For other techs in which we have experience, please check here.`,
+}
+
+export const profile = {
+    jp: `工学系の大学を卒業するも就職活動に失敗し, 家電量販店のパソコン売り場でコンピュータの知識を磨きながら接客スキルを習得。当時はパッケージソフトウェアやコンピュータ書籍売り場を担当。日常の接客のみならず, 売れ残り品のたたき売りオークションなど 店内イベントでも活躍し, 店舗の売上に貢献する。
+
+
+    その後, パソコン好きが高じて製造業の社内システム部門に中途採用で潜り込み, そのまま系列のSI会社にシステムエンジニアとして勤務。少数精鋭の現場においては, 要件定義からテスト, エンドユーザサポートまでありとあらゆる工程の業務を担当し, システム開発の経験を積む。
+    
+
+    
+    第２子の誕生を機に 10年以上勤めたSI会社を辞め, 地元のソフトウェア会社に転職。
+
+    さらに5年の紆余曲折を経て, 現在はフリーランスとして独立。コーヒーをすすり, プロテインバーをかじり, 妻の用意する生野菜をモシャりながら VSCode で <code>go test</code> したり, <code>wrangler publish -e production</code> したり, <code>git reset --HARD origin/develop</code> したり, <code>docker</code> の <code>compose</code> を <code>up -d</code> したりする毎日。
+    `,
+    en: `I live in Mito City, Ibaraki Pref. in Japan.
+    
+    After graduating from an engineering college, then
+    
+    clerk at a computer store, in-house SE at a manufacturing company, software engineer(system architect, programmer, tester, user support...etc), 
+    
+    I have experienced a variety of jobs.
+    
+    
+    I am currently a freelance web engineer.`,
+}
+
+export const closing = {
+    jp: `最後までお付き合いいただき, ありがとうございました。
+    忍者をクリックすると忍法でトップに戻ります。`,
+    en: `Thank you for reading to the end.
+    Click on the ninja to return to the top with the ninja method (aka. "NIN-PO").`,
+}

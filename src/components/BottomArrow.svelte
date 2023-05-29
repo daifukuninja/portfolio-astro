59c146a891fb9fb6126fe2131152214fcbf25bf2
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-05-29 14:17:21 +0000

    modify: arrowの位置調整

--- src/components/BottomArrow.svelte
+++ src/components/BottomArrow.svelte
@@ -2,20 +2,9 @@
 </script>
 
 <div class="wrapper">
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-        viewBox="0 0 24 24"
-        style="fill: rgba(192, 192, 192, 1);"
-        class="arrow"
-    >
-        <path
-            d="M16.939 10.939 12 15.879l-4.939-4.94-2.122 2.122L12 20.121l7.061-7.06z"
-        />
-        <path
-            d="M16.939 3.939 12 8.879l-4.939-4.94-2.122 2.122L12 13.121l7.061-7.06z"
-        />
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(192, 192, 192, 1);" class="arrow">
+        <path d="M16.939 10.939 12 15.879l-4.939-4.94-2.122 2.122L12 20.121l7.061-7.06z" />
+        <path d="M16.939 3.939 12 8.879l-4.939-4.94-2.122 2.122L12 13.121l7.061-7.06z" />
     </svg>
 </div>
 
@@ -50,7 +39,7 @@
     }
     @media (min-width: 768px) {
         .wrapper {
-            top: 40%;
+            top: 10%;
         }
     }
 </style>

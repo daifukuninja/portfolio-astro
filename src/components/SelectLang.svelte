fb35b1cccdfa95f3cab933afb44ef340d5552178
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-05-29 14:17:00 +0000

    update: 言語セレクタを多言語対応、リストのスタイル調整

--- src/components/SelectLang.svelte
+++ src/components/SelectLang.svelte
@@ -4,33 +4,44 @@
 
   let langKey: langType;
 
-  langStore.subscribe(value => {
+  langStore.subscribe((value) => {
     langKey = value;
   });
 
   const switchLanguage = (l: langType) => {
     return () => {
-      langStore.update(_ => {
+      langStore.update((_) => {
         return l;
       });
     };
   };
 </script>
 
-{#if langKey == "jp"}
-  <a href={null} on:click={switchLanguage("en")}>Click here for English.</a>
-  <a href={null} on:click={switchLanguage("es")}>Haga clic aquí para español.</a>
-{:else if langKey == "en"}
-  <a href={null} on:click={switchLanguage("es")}>Haga clic aquí para español.</a>
-  <a href={null} on:click={switchLanguage("jp")}>日本語はこちら</a>
-{:else if langKey == "es"}
-  <a href={null} on:click={switchLanguage("en")}>Click here for English.</a>
-  <a href={null} on:click={switchLanguage("jp")}>日本語はこちら</a>
-{/if}
+<ul>
+  {#if langKey != "jp"}
+    <li><a href={null} on:click={switchLanguage("jp")}>日本語はこちら</a></li>
+  {/if}
+  {#if langKey != "en"}
+    <li><a href={null} on:click={switchLanguage("en")}>Click here for English.</a></li>
+  {/if}
+  {#if langKey != "es"}
+    <li><a href={null} on:click={switchLanguage("es")}>Haga clic aquí para español.</a></li>
+  {/if}
+</ul>
 
 <style>
   a {
     cursor: pointer;
     text-decoration: underline;
+    text-underline-offset: 0.8ex;
+    color: white;
+    font-family: "Noto Sans JP";
+  }
+
+  ul {
+    list-style: none;
+    border-left: solid 3px whitesmoke;
+    padding-left: 1em;
+    padding-bottom: 0.3em;
   }
 </style>
